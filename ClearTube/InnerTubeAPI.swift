b8e7be1b4f4b3f2eeaa18462a8eec43cdc598bc4
Peter Jang <peter.x.jang@gmail.com>
2024-09-17 20:24:52 -0500

    Rearrange functions for clarity

--- ClearTube/InnerTubeAPI.swift
+++ ClearTube/InnerTubeAPI.swift
@@ -9,61 +9,6 @@ public final class InnerTubeAPI {
         self.session = session
     }
 
-    func requestUrl(for string: String, with queryItems: [URLQueryItem]? = nil) -> URL? {
-        guard var url = URL(string: string, relativeTo: baseUrl) else {
-            return nil
-        }
-        if let queryItems = queryItems {
-            if #available(iOS 16, macOS 13, tvOS 16, *) {
-                url.append(queryItems: queryItems)
-            } else {
-                guard var components = URLComponents(url: url, resolvingAgainstBaseURL: true) else {
-                    return nil
-                }
-                components.queryItems = queryItems
-                return components.url
-            }
-        }
-        return url
-    }
-
-    func request(for string: String, with queryItems: [URLQueryItem]? = nil) async throws -> (Data, URLResponse) {
-        guard let url = requestUrl(for: string, with: queryItems) else {
-            throw APIError.urlCreation
-        }
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36", forHTTPHeaderField: "User-Agent")
-        request.addValue("1", forHTTPHeaderField: "X-YouTube-Client-Name")
-        request.addValue("2.20230728.00.00", forHTTPHeaderField: "X-YouTube-Client-Version")
-        request.httpBody = try JSONEncoder().encode(
-            RequestBody(
-                context: RequestBody.Context(
-                    client: RequestBody.Context.Client(
-                        clientName: "IOS",
-                        clientVersion: "19.16.3",
-                        hl: "en",
-                        gl: "US",
-                        deviceMake: "Apple",
-                        deviceModel: "iPhone",
-                        experimentIds: [],
-                        utcOffsetMinutes: 0
-                    ),
-                    user: RequestBody.Context.User(
-                        lockedSafetyMode: false
-                    ),
-                    request: RequestBody.Context.RequestContext(
-                        useSsl: true,
-                        internalExperimentFlags: [],
-                        consistencyTokenJars: []
-                    )
-                )
-            )
-        )
-        return try await session.data(for: request)
-    }
-
     func playerEndpoint(for id: String) async throws -> PlayerResponse {
         guard let idPath = id.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) else {
             throw APIError.urlCreation
@@ -105,8 +50,92 @@ public final class InnerTubeAPI {
             throw error
         }
     }
+    
+    func video(for id: String) async throws -> VideoObject {
+        async let playerTask = playerEndpoint(for: id)
+        async let nextTask = nextEndpoint(for: id)
+        do {
+            let (json, nextJson) = try await(playerTask, nextTask)
+            let recommendedVideos = extractRecommendedVideos(json: nextJson)
+            return VideoObject(
+                title: json.videoDetails.title,
+                videoId: json.videoDetails.videoId,
+                lengthSeconds: Int(json.videoDetails.lengthSeconds) ?? 0,
+                videoThumbnails: json.videoDetails.thumbnail.thumbnails,
+                viewCount: Int64(json.videoDetails.viewCount),
+                author: json.videoDetails.author,
+                authorId: json.videoDetails.channelId,
+                hlsUrl: json.streamingData.hlsManifestUrl,
+                recommendedVideos: recommendedVideos
+            )
+        } catch {
+            print("An error occurred: \(error.localizedDescription)")
+            print("Error details: \(error)")
+            throw error
+        }
+    }
 
-    func extractRecommendedVideos(json: NextResponse) -> [VideoObject.RecommendedVideoObject] {
+    func search(query: String, page: Int32) async throws -> [SearchObject.Result] {
+        let json = try await ClearTubeApp.innerTubeClient.searchEndpoint(query: query)
+        return extractSearchResults(json: json)
+    }
+
+    private func requestUrl(for string: String, with queryItems: [URLQueryItem]? = nil) -> URL? {
+        guard var url = URL(string: string, relativeTo: baseUrl) else {
+            return nil
+        }
+        if let queryItems = queryItems {
+            if #available(iOS 16, macOS 13, tvOS 16, *) {
+                url.append(queryItems: queryItems)
+            } else {
+                guard var components = URLComponents(url: url, resolvingAgainstBaseURL: true) else {
+                    return nil
+                }
+                components.queryItems = queryItems
+                return components.url
+            }
+        }
+        return url
+    }
+
+    private func request(for string: String, with queryItems: [URLQueryItem]? = nil) async throws -> (Data, URLResponse) {
+        guard let url = requestUrl(for: string, with: queryItems) else {
+            throw APIError.urlCreation
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36", forHTTPHeaderField: "User-Agent")
+        request.addValue("1", forHTTPHeaderField: "X-YouTube-Client-Name")
+        request.addValue("2.20230728.00.00", forHTTPHeaderField: "X-YouTube-Client-Version")
+        request.httpBody = try JSONEncoder().encode(
+            RequestBody(
+                context: RequestBody.Context(
+                    client: RequestBody.Context.Client(
+                        clientName: "IOS",
+                        clientVersion: "19.16.3",
+                        hl: "en",
+                        gl: "US",
+                        deviceMake: "Apple",
+                        deviceModel: "iPhone",
+                        experimentIds: [],
+                        utcOffsetMinutes: 0
+                    ),
+                    user: RequestBody.Context.User(
+                        lockedSafetyMode: false
+                    ),
+                    request: RequestBody.Context.RequestContext(
+                        useSsl: true,
+                        internalExperimentFlags: [],
+                        consistencyTokenJars: []
+                    )
+                )
+            )
+        )
+        return try await session.data(for: request)
+    }
+
+    private func extractRecommendedVideos(json: NextResponse) -> [VideoObject.RecommendedVideoObject] {
         var recommendedVideos: [VideoObject.RecommendedVideoObject] = []
         for content in json.contents.singleColumnWatchNextResults.results.results.contents {
             if content.shelfRenderer != nil {
@@ -133,7 +162,7 @@ public final class InnerTubeAPI {
         return recommendedVideos
     }
 
-    func extractSearchResults(json: SearchResponse) -> [SearchObject.Result] {
+    private func extractSearchResults(json: SearchResponse) -> [SearchObject.Result] {
         var results: [SearchObject.Result] = []
         for contentJson in json.contents.sectionListRenderer.contents {
             // CHANNEL
@@ -182,7 +211,7 @@ public final class InnerTubeAPI {
         return results
     }
 
-    func timeStringToSeconds(_ timeString: String) -> Int? {
+    private func timeStringToSeconds(_ timeString: String) -> Int? {
         let components = timeString.split(separator: ":").map { Int($0) }
         guard components.allSatisfy({ $0 != nil }) else {
             return nil
@@ -201,34 +230,5 @@ public final class InnerTubeAPI {
         default: // Invalid format
             return nil
         }
-    }
-
-    func video(for id: String) async throws -> VideoObject {
-        async let playerTask = playerEndpoint(for: id)
-        async let nextTask = nextEndpoint(for: id)
-        do {
-            let (json, nextJson) = try await(playerTask, nextTask)
-            let recommendedVideos = extractRecommendedVideos(json: nextJson)
-            return VideoObject(
-                title: json.videoDetails.title,
-                videoId: json.videoDetails.videoId,
-                lengthSeconds: Int(json.videoDetails.lengthSeconds) ?? 0,
-                videoThumbnails: json.videoDetails.thumbnail.thumbnails,
-                viewCount: Int64(json.videoDetails.viewCount),
-                author: json.videoDetails.author,
-                authorId: json.videoDetails.channelId,
-                hlsUrl: json.streamingData.hlsManifestUrl,
-                recommendedVideos: recommendedVideos
-            )
-        } catch {
-            print("An error occurred: \(error.localizedDescription)")
-            print("Error details: \(error)")
-            throw error
-        }
-    }
-
-    func search(query: String, page: Int32) async throws -> [SearchObject.Result] {
-        let json = try await ClearTubeApp.innerTubeClient.searchEndpoint(query: query)
-        return extractSearchResults(json: json)
     }
 }
